6a18567580c1de707159fdffb6702b19b619f964
FelipeMunevar <felipemunevarn@gmail.com>
2022-07-19 22:58:30 -0300

    Initial commit

---
+++ src/main/java/org/alkemy/campus/DisneyAPIRest/DisneyApiRestApplication.java
@@ -0,0 +1,13 @@
+package org.alkemy.campus.DisneyAPIRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alkemy/campus/DisneyAPIRest/DisneyApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alkemy.campus.DisneyAPIRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
